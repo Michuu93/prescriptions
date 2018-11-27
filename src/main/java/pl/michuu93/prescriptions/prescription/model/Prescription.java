e98b27753a4aa9826a2f3256f900aa471c0002f0
Michał H <michuu93@gmail.com>
2018-11-27 20:21:21 +0100

    add prescription model

--- src/main/java/pl/michuu93/prescriptions/prescription/model/Prescription.java
+++ src/main/java/pl/michuu93/prescriptions/prescription/model/Prescription.java
@@ -1,20 +1,21 @@
 package pl.michuu93.prescriptions.prescription.model;
 
 import lombok.Data;
+import pl.michuu93.prescriptions.office.OfficeData;
+import pl.michuu93.prescriptions.patient.model.Patient;
 
 import javax.persistence.*;
 
 @Data
 @Entity
-@Table(indexes = {
-        @Index(columnList = "patientId")
-})
 public class Prescription {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String id;
-
-    private String patientId;
-
-
+    @ManyToOne(targetEntity = Patient.class)
+    private Patient patient;
+    private OfficeData officeData;
+    private PrescriptionNumber prescriptionNumber;
+    @Enumerated(EnumType.STRING)
+    private PrescriptionPermissions additionalPermissions = PrescriptionPermissions.X;
 }
---
+++ src/main/java/pl/michuu93/prescriptions/prescription/model/PrescriptionPermissions.java
@@ -0,0 +1,5 @@
+package pl.michuu93.prescriptions.prescription.model;
+
+public enum PrescriptionPermissions {
+    X, S, IB, IW, ZK, AZ, WP, PO, CN, DN, IN
+}
